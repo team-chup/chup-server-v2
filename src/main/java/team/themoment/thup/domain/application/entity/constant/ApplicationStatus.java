3af69c9d98d1d008888b016835c64804c248ea5b
hongjm0912~ <s25036@gsm.hs.kr>
2026-08-29 15:38:38 +0900

    add(application): 지원 상태에 서류 탈락 추가

--- src/main/java/team/themoment/thup/domain/application/entity/constant/ApplicationStatus.java
+++ src/main/java/team/themoment/thup/domain/application/entity/constant/ApplicationStatus.java
@@ -2,6 +2,7 @@ package team.themoment.thup.domain.application.entity.constant;
 
 public enum ApplicationStatus {
     APPLIED,             // 지원 접수 / 결과 대기
+    DOCUMENT_FAILED,     // 서류 탈락
     INTERVIEW_SCHEDULED, // 면접 예정
     PASSED,              // 최종 합격
     FAILED               // 면접 탈락
